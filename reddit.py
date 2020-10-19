a5c1c233c2a5ffb78b5bf3deff78d5374d8eed77
hauh <hauh@ya.ru>
2020-10-19 05:11:57 +0300

    Check subreddit for new posts

---
+++ reddit.py
@@ -0,0 +1,43 @@
+"""Get new posts from Reddit."""
+
+import os
+import requests
+
+
+HEADER = {
+	'User-Agent':
+		"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.6.01001)"
+}
+SUBREDDIT_URL = f"https://reddit.com/r/{os.environ['subreddit']}"
+NEW_POSTS_URL = SUBREDDIT_URL + "/new.json"
+SEEN_POSTS_IDS = set()
+
+
+def get_post(post_id):
+	if not (response := requests.get(
+		f"{SUBREDDIT_URL}/{post_id}.json", headers=HEADER)).ok:
+		response.raise_for_status()
+	post = response.json()[0]['data']['children'][0]['data']
+	if post_url := post.get('url_overridden_by_dest'):
+		message = f"<a href={post_url}>{post['title']}</a>"
+	else:
+		message = f"<b>{post['title']}</b>"
+	if post_text := post.get('selftext'):
+		message += post_text
+	return message
+
+
+def new_posts_checker():
+	if not (response := requests.get(NEW_POSTS_URL, headers=HEADER)).ok:
+		response.raise_for_status()
+	new_posts = response.json()['data']['children']
+	new_posts_ids = set(post['data']['id'] for post in new_posts)
+	if not SEEN_POSTS_IDS:
+		yield get_post(new_posts[0]['data']['id'])
+	else:
+		for post_id in new_posts_ids:
+			if post_id not in SEEN_POSTS_IDS:
+				yield get_post(post_id)
+	SEEN_POSTS_IDS.clear()
+	SEEN_POSTS_IDS.update(new_posts_ids)
+	return
